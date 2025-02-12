15c435303cafc8b75df6d4a870c4e576ccd5b36c
Laborde Marius <144605008+mariuslbrde@users.noreply.github.com>
2025-02-12 19:08:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
-document.addEventListener('mousemove', function(e) {
-  const text = document.querySelector('.gradient-text');
-  const text2 = document.querySelector('.gradient-text2');
-  const headers = document.querySelectorAll('header h1'); // Sélectionne tous les h1
-  const { clientX, clientY } = e;
-  const { innerWidth, innerHeight } = window;
+const isMobile = window.matchMedia("(max-width: 768px)").matches;
 
-  const xPercent = clientX / innerWidth;
-  const yPercent = clientY / innerHeight;
+if (!isMobile) {
+  document.addEventListener('mousemove', function(e) {
+    const text = document.querySelector('.gradient-text');
+    const text2 = document.querySelector('.gradient-text2');
+    const headers = document.querySelectorAll('header h1');
+    const { clientX, clientY } = e;
+    const { innerWidth, innerHeight } = window;
 
-  const xOffset = (xPercent - 0.5) * 100;
-  const yOffset = (yPercent - 0.5) * 100;
+    const xPercent = clientX / innerWidth;
+    const yPercent = clientY / innerHeight;
 
-  if (text) text.style.backgroundPosition = `${50 - xOffset}% ${50 - yOffset}%`;
-  if (text2) text2.style.backgroundPosition = `${50 - xOffset}% ${50 - yOffset}%`;
+    const xOffset = (xPercent - 0.5) * 100;
+    const yOffset = (yPercent - 0.5) * 100;
 
-  // Boucle sur tous les h1 pour animer le dégradé
-  headers.forEach(header => {
-    header.style.backgroundPosition = `${50 - xOffset}% ${50 - yOffset}%`;
+    if (text) text.style.backgroundPosition = `${50 - xOffset}% ${50 - yOffset}%`;
+    if (text2) text2.style.backgroundPosition = `${50 - xOffset}% ${50 - yOffset}%`;
+
+    headers.forEach(header => {
+      header.style.backgroundPosition = `${50 - xOffset}% ${50 - yOffset}%`;
+    });
   });
-});
 
-document.addEventListener('mousemove', function(e) {
-  const light = document.querySelector('.mouse-light');
-  if (light) {
-    light.style.left = `${e.pageX}px`;
-    light.style.top = `${e.pageY}px`;
-  }
-});
+  document.addEventListener('mousemove', function(e) {
+    const light = document.querySelector('.mouse-light');
+    if (light) {
+      light.style.left = `${e.pageX}px`;
+      light.style.top = `${e.pageY}px`;
+    }
+  });
+}
+
